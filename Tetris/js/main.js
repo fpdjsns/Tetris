e4d6ed9aa34ecba2d0943a5a0623fe91dd65aaae
fpdjsns.github.io <withham1@naver.com>
2018-07-25 21:01:41 +0900

    Next block type random change

--- Tetris/js/main.js
+++ Tetris/js/main.js
@@ -10,16 +10,19 @@ var blockType = [
 
 var nowBlock;
 var canvas = document.getElementById('game');
+var nextBlockType = Math.floor(Math.random() * blockType.length);
 
 const BEGIN_X = 30;
 const BEGIN_Y = 30;
-const SPEED = 5;
+const SPEED = 50;
 const WIDTH = canvas.clientWidth;
 const HEIGHT = canvas.clientHeight;
 const CANVAS_LEFT = canvas.clientLeft;
 const CANVAS_RIGHT = canvas.clientLeft + WIDTH;
 const CANVAS_TOP = canvas.clientTop;
 const CANVAS_BOTTOM = canvas.clientTop + HEIGHT;
+
+const WAIT_NEXTBLOCK_TIME = 500;
 
 if(canvas.getContext){
     var ctx = canvas.getContext('2d');
@@ -29,16 +32,17 @@ if(canvas.getContext){
 
 $(window).load(function(){
     console.log('load');
-    drawNewBlock(2);
+    drawNewBlock();
 });
 
 setInterval(function(){
     nowBlock.drawBlock(nowBlock.x, nowBlock.y+SPEED);
 }, 500);
 
-var drawNewBlock = function(blockTypeIndex){
-    nowBlock = new Block(blockTypeIndex, BEGIN_X, BEGIN_Y);
+var drawNewBlock = function(){
+    nowBlock = new Block(nextBlockType, BEGIN_X, BEGIN_Y);
     nowBlock.drawBlock(BEGIN_X, BEGIN_Y);
+    nextBlockType = Math.floor(Math.random() * blockType.length);
 }
 
 $(document).keydown(function(e){
@@ -59,15 +63,19 @@ function Block(blockTypeIndex, x, y){
     this.y = y;
     this.width = 50;
     this.height = 70;
-    ctx.fillStyle = this.type.color;
-
+    
     this.drawBlock = function(nx, ny){ 
         ctx.clearRect(this.x, this.y, this.width, this.height);
         if(nx < CANVAS_LEFT || nx + this.width> CANVAS_RIGHT) nx = this.x;
         if(ny + this.height > CANVAS_BOTTOM) {
             ny = this.y;
-            drawNewBlock(2);
+            setTimeout(
+                drawNewBlock(), 
+                WAIT_NEXTBLOCK_TIME);
         }
+
+        // Change colors just before drawing to avoid affecting the previous or next block color.
+        ctx.fillStyle = this.type.color;
         ctx.fillRect(nx, ny, this.width,this.height); 
         console.log(nx + ','+ny);
         this.x = nx;
